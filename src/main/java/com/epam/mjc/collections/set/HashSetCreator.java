cbcb256566f39856c8a617dd368c3eec956bbdfd
Korotkova Julia <korotkovayuliav@gmail.com>
2022-11-22 13:02:02 +0100

    done

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -1,9 +1,24 @@
 package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> set = new LinkedHashSet<>();
+        for (Integer element : sourceList) {
+            if (element % 2 == 0) {
+                while (element % 2 == 0) {
+                    set.add(element);
+                    element /= 2;
+                }
+                set.add(element);
+            } else {
+                set.add(element);
+                set.add(element * 2);
+            }
+        }
+        return set;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,15 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (Integer element : sourceList) {
+            element *= element;
+            set.add(element);
+        }
+        return set.subSet(lowerBound, true, upperBound, true);
     }
 }
